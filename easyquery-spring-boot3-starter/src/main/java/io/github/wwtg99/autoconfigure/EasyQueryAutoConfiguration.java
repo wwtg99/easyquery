b19bb81a85b1a3207d3b16be51edbfd456d0ff2a
wwtg99 <wwtg99@gmail.com>
2025-08-25 20:20:57 +0800

    update auto configuration

--- easyquery-spring-boot3-starter/src/main/java/io/github/wwtg99/autoconfigure/EasyQueryAutoConfiguration.java
+++ easyquery-spring-boot3-starter/src/main/java/io/github/wwtg99/autoconfigure/EasyQueryAutoConfiguration.java
@@ -4,13 +4,15 @@ import io.github.wwtg99.core.extractor.ExtractorHolder;
 import io.github.wwtg99.core.extractor.FilterExtractor;
 import io.github.wwtg99.core.extractor.SearcherExtractor;
 import io.github.wwtg99.core.extractor.SorterExtractor;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@AutoConfiguration
 public class EasyQueryAutoConfiguration {
 
     @Bean
+    @ConditionalOnMissingBean
     public ExtractorHolder extractorHolder() {
         return new ExtractorHolder()
                 .addExtractor(new FilterExtractor())
